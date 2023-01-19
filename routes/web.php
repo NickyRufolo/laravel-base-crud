c7f4775590b4ecbcffe767a08f6485d7763deb2d
NikiRufolo <nikirufolo@gmail.com>
2023-01-19 18:41:56 +0100

    crud_laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index')->name('home');
+Route::get('/', 'comics')->name('');
